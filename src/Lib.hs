407d12ab9b56dabe31762ea8d46d2766cbc292d7
Jappie Klooster <jappieklooster@hotmail.com>
2020-11-13 21:58:50 +0100

    Builds

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,24 @@
 module Lib
-  ( libF
-  )
 where
 
-import           Control.Monad.IO.Class
+import Data.Primitive.SmallArray
+import Data.Hashable
+import Data.Word
 
-libF :: MonadIO m => m ()
-libF = liftIO $ putStrLn "hello, world ss"
+-- TODO https://hackage.haskell.org/package/vec-0.3/docs/Data-Vec-Lazy.html
+-- https://github.com/lehins/massiv
+
+
+data Hamt key value = Empty
+                    | KeyVal key value
+                    | SubTry (SmallArray (Hamt key value))
+
+-- this is the size of the smallarray
+-- not related to the size of the bitmap of the hash!
+someBranchingFactor = 32
+
+lookup :: Hashable key => key -> Hamt key value -> Maybe value
+lookup key _ = Nothing
+  where
+    keyHash :: Word32
+    keyHash = toEnum $ hash key
